40d08d90ecd4f6a6340c8634ce32c28a28bfebc6
Felipe <fdario646@gmail.com>
2023-05-05 12:52:04 -0300

    adding media queries

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,51 @@
+const form = document.querySelector('form');
 const calculateButton = document.querySelector("button");
 const res = document.getElementById('res');
 const input = document.querySelector("#inputValue");
+
 calculateButton.addEventListener("click", result);
 
+let clearButton = document.createElement('button');
+clearButton.textContent = `Limpar`;
+clearButton.addEventListener("click", clear);
 
 function result(e) {
     e.preventDefault();
     res.innerHTML = ``;
     fetch('http://economia.awesomeapi.com.br/last/USD-BRL,GBP-BRL')
-    .then(response => response.json())
-    .then(data => {
-        if (input.value != '') {
-            let ul = document.createElement('ul');
-            let li = document.createElement('li');
-            let BRL = document.createElement('p');
-            BRL.innerHTML = `Real = R$ ${input.value}`;
-            let li2 = document.createElement('li');
-            let USD = document.createElement('p');
-            USD.innerHTML = `USD (Dólar americano) = $ ${parseFloat(input.value/data.USDBRL.bid).toFixed(2)}`;
-            let li3 = document.createElement('li');
-            let GBP = document.createElement('p');
-            GBP.innerHTML = `GBP (Libra esterlina) = £$ ${parseFloat(input.value/data.GBPBRL.bid).toFixed(2)}`;          
-            res.appendChild(ul);
-            li.appendChild(BRL);
-            ul.appendChild(li);
-            li2.appendChild(USD);
-            ul.appendChild(li2);
-            li3.appendChild(GBP);
-            ul.appendChild(li3);
-        }
-        else {
-            alert("Type some value");
-        }
-    })
-    .catch(err => console.log(err.json()));
+        .then(response => response.json())
+        .then(data => {
+            if (input.value != '' && input.value != 0 && input.value > 0) {
+                form.appendChild(clearButton);
+                let ul = document.createElement('ul');
+                let li = document.createElement('li');
+                let BRL = document.createElement('p');
+                BRL.innerHTML = `Real = R$ ${input.value}`;
+                let li2 = document.createElement('li');
+                let USD = document.createElement('p');
+                USD.innerHTML = `USD (Dólar americano) = $ ${parseFloat(input.value / data.USDBRL.bid).toFixed(2)}`;
+                let li3 = document.createElement('li');
+                let GBP = document.createElement('p');
+                GBP.innerHTML = `GBP (Libra esterlina) = £$ ${parseFloat(input.value / data.GBPBRL.bid).toFixed(2)}`;
+                
+                li.appendChild(BRL);
+                ul.appendChild(li);
+                
+                li2.appendChild(USD);
+                ul.appendChild(li2);
+                
+                li3.appendChild(GBP);
+                ul.appendChild(li3);
 
+                res.appendChild(ul);
+            }
+            else {
+                alert("Type the correct value!");
+            }
+        })
+        .catch(err => console.log(err.json()));
+}
+function clear() { 
+    input.nodeValue = `0`;
+    res.innerHTML = ``;
 }
